c90b442f7893e888fade03bfe300579c05ea1ff4
dcollins4096 <dcollins4096@gmail.com>
2023-05-26 11:48:54 -0400

    first

---
+++ run/results.js
@@ -0,0 +1,19 @@
+test_data = [
+  {
+    "results": [], 
+    "name": "AdiabaticExpansion"
+  }, 
+  {
+    "results": [], 
+    "name": "ZeldovichPancake"
+  }, 
+  {
+    "results": [], 
+    "name": "DrivenTurbulence3D"
+  }, 
+  {
+    "results": [], 
+    "name": "MHDCTOrszagTangAMR"
+  }
+];
+current_set = '8d7c9b6d9d68+ (week-of-code) tip';
